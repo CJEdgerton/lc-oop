70acf4478a73aa7496a46200318bf376412a6743
CJEdgerton <cjedgerton@gmail.com>
2016-05-21 12:17:15 +0000

    Review

---
+++ Classes/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+class Task
+{
+	public $description;
+
+	public $completed = false;
+
+	public function __construct($description) {
+		$this->description = $description;
+	}
+
+	public function complete() {
+		$this->completed = true;
+	}	
+}
+
+$task = new Task('Learn OOP');
+
+var_dump($task->description);
+
+$task->complete();
+
+var_dump($task->completed);
